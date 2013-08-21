5f45a211a4c2c8c4da5c1d46aa81ec414f5f8f61
Kakodaemon <loader.conf@yandex.ru>
2013-08-21 17:06:12 +0700

    Just testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3testapp::Application.config.secret_token = '2689a1a0a382b797cee0c506f0af21d4f163b96609f2c75ea8c99e3758122b06c8a9b3c06b58bd8b7d067d6a28d67b99d5ce4d3b07d4a54d4d87a5259363d417'
